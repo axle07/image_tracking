a149e0da18de8898be46a3d13e9890884dfe32b9
Alex <joeblack78006@yahoo.com>
2013-09-16 10:16:47 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageTracking::Application.config.secret_key_base = 'cd161fc7c0b13322adc345c35a6628f8ab43eaf8637ef2e5cc008276abb157aaf57bd9a6b872598ef2d099236af6fc903de8b5db1d5581b25ca4afe2d370923c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImageTracking::Application.config.session_store :cookie_store, key: '_image_tracking_session'
